c4fdd08c1b1267fa47d2bb7dfa119171403ae3a0
Fincorelabs <fincorelabs@gmail.com>
2025-06-26 22:58:10 -0300

    Create LandingPage.jsx

---
+++ LandingPage.jsx
@@ -0,0 +1,8 @@
+export default function LandingPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "80px" }}>
+      <h1>🚀 Landing Page da Fincore Labs</h1>
+      <p>Estamos construindo algo incrível. Aguarde novidades!</p>
+    </div>
+  );
+}

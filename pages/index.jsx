e64eeb5f34b8469f5b542d3052b25b5d2e91a88b
Fincorelabs <fincorelabs@gmail.com>
2025-06-28 00:10:20 -0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,5 @@
-import IdentidadeVisual from '../components/sections/IdentidadeVisual';
-import OProblema from '../components/sections/OProblema';
+import LandingPage from './LandingPage';
 
 export default function Home() {
-  return (
-    <>
-      <IdentidadeVisual />
-      <OProblema />
-    </>
-  );
+  return <LandingPage />;
 }

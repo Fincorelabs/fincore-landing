2dbb2373bf6e9911338420088b3a63a1886d8fe8
Fincorelabs <fincorelabs@gmail.com>
2025-06-28 00:09:14 -0300

    Update LandingPage.jsx

--- LandingPage.jsx
+++ LandingPage.jsx
@@ -1,14 +1,11 @@
-import OProblema from '../components/sections/OProblema';
 import IdentidadeVisual from '../components/sections/IdentidadeVisual';
-
-
+import OProblema from '../components/sections/OProblema';
 
 export default function Home() {
   return (
     <>
       <IdentidadeVisual />
       <OProblema />
-      {/* próximos blocos aqui... */}
     </>
   );
 }

a8da97241f193e7356134f1728b728fcccb45ff6
Fincorelabs <fincorelabs@gmail.com>
2025-06-29 20:12:34 -0300

    Create LandingPages.jsx

---
+++ components/sections/LandingPages.jsx
@@ -0,0 +1,21 @@
+import IdentidadeVisual from "../components/sections/IdentidadeVisual";
+import OProblema from "../components/sections/OProblema";
+import AOportunidade from "../components/sections/AOportunidade";
+import AEstrutura from "../components/sections/AEstrutura";
+import OPerfilQueProcuramos from "../components/sections/OPerfilQueProcuramos";
+import OConvite from "../components/sections/OConvite";
+import ProximosPassos from "../components/sections/ProximosPassos";
+
+export default function LandingPage() {
+  return (
+    <main>
+      <IdentidadeVisual />
+      <OProblema />
+      <AOportunidade />
+      <AEstrutura />
+      <OPerfilQueProcuramos />
+      <OConvite />
+      <ProximosPassos />
+    </main>
+  );
+}

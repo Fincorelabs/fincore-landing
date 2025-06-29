3c213d2188c888578be622f1c37309da9c07f7de
Fincorelabs <fincorelabs@gmail.com>
2025-06-28 22:21:25 -0300

    Update LandingPage.jsx

--- LandingPage.jsx
+++ LandingPage.jsx
@@ -1,10 +1,8 @@
-import IdentidadeVisual from "../components/sections/IdentidadeVisual";
-import OProblema from "../components/sections/OProblema";
-import AOportunidade from "../components/sections/AOportunidade";
-import AEstrutura from "../components/sections/AEstrutura";
-import OPerfilQueProcuramos from "../components/sections/OPerfilQueProcuramos";
-import OConvite from "../components/sections/OConvite";
-import ProximosPassos from "../components/sections/ProximosPassos";
+import IdentidadeVisual from '../components/sections/IdentidadeVisual';
+import OProblema from '../components/sections/OProblema';
+import AOportunidade from '../components/sections/AOportunidade';
+import AEstrutura from '../components/sections/AEstrutura';
+import OPerfilQueProcuramos from '../components/sections/OPerfilQueProcuramos';
 
 export default function LandingPage() {
   return (
@@ -14,9 +12,6 @@ export default function LandingPage() {
       <AOportunidade />
       <AEstrutura />
       <OPerfilQueProcuramos />
-      <OConvite />
-      <ProximosPassos />
     </>
   );
 }
-

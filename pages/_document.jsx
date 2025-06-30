e26f0056ef08047afc34c0ac063fdc72cc84ed15
Fincorelabs <fincorelabs@gmail.com>
2025-06-29 21:10:19 -0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,21 +5,11 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-        {/* Fonte personalizada (exemplo com Inter) */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-
-        {/* Metadados básicos */}
-        <meta charSet="UTF-8" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Landing page confidencial da Fincore Labs" />
-        <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
       </Head>
-      <body className="bg-white text-gray-900">
+      <body>
         <Main />
         <NextScript />
       </body>
